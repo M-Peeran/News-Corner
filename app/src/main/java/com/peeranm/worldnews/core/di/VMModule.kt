cd8488825b40b74a86b0f9e1b07b0079d062d05b
M-Peeran <peeramakrabbi@gmail.com>
2022-11-12 01:18:33 +0530

    Added @Provides annotation

--- app/src/main/java/com/peeranm/worldnews/core/di/VMModule.kt
+++ app/src/main/java/com/peeranm/worldnews/core/di/VMModule.kt
@@ -10,6 +10,7 @@ import com.peeranm.worldnews.newsfeed.usecase.GetTrendingNewsUseCase
 import com.peeranm.worldnews.searchnews.data.repository.NewsSearchRepository
 import com.peeranm.worldnews.searchnews.usecase.SearchNewsUseCase
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ViewModelComponent
 import dagger.hilt.android.scopes.ViewModelScoped
@@ -19,31 +20,37 @@ import dagger.hilt.android.scopes.ViewModelScoped
 class VMModule {
 
     @ViewModelScoped
+    @Provides
     fun provideGetTrendingNewsUseCase(repository: NewsRepository): GetTrendingNewsUseCase {
         return GetTrendingNewsUseCase(repository)
     }
 
     @ViewModelScoped
+    @Provides
     fun provideSearchNewsUseCase(repository: NewsSearchRepository): SearchNewsUseCase {
         return SearchNewsUseCase(repository)
     }
 
     @ViewModelScoped
+    @Provides
     fun provideInsertFavArticleUseCase(repository: FavouriteArticleRepository): InsertFavArticleUseCase {
         return InsertFavArticleUseCase(repository)
     }
 
     @ViewModelScoped
+    @Provides
     fun provideGetFavArticleUseCase(repository: FavouriteArticleRepository): GetFavArticleUseCase {
         return GetFavArticleUseCase(repository)
     }
 
     @ViewModelScoped
+    @Provides
     fun provideGetFavArticlesUseCase(repository: FavouriteArticleRepository): GetFavArticlesUseCase {
         return GetFavArticlesUseCase(repository)
     }
 
     @ViewModelScoped
+    @Provides
     fun provideDeleteFavArticleUseCase(repository: FavouriteArticleRepository): DeleteFavArticleUseCase {
         return DeleteFavArticleUseCase(repository)
     }
