1ccedeeaa4cfc78de3d87c2d5fc7a73f4738e82c
M-Peeran <peeramakrabbi@gmail.com>
2022-11-30 13:46:46 +0530

    Added a color & some constants

--- app/src/main/java/com/peeranm/newscorner/core/constants/Constants.kt
+++ app/src/main/java/com/peeranm/newscorner/core/constants/Constants.kt
@@ -6,11 +6,11 @@ object Constants {
     const val MAX_ARTICLES_FOR_PAGINATION = 100
     const val INITIAL_PAGE = 1
     const val SEARCH_QUERY = "searchQuery"
-    const val ARG_ARTICLE = "article"
     const val TAG_DIALOG_COUNTRY = "countryDialog"
     const val KEY_COUNTRY_DIALOG_RESULT_LISTENER = "countryDialogResultListener"
     const val ARG_COUNTRY_DIALOG_RESULT = "countryDialogResult"
     const val MESSAGE_SOMETHING_WENT_WRONG = "Something went wrong. Please try again later."
     const val MESSAGE_NO_INTERNET_CONNECTION = "Internet connection is not available! Please check your connection."
-    const val SOURCE_UNKNOWN = "Unknown Source"
+    const val ARG_ARTICLE = "article"
+    const val ARG_FAV_ARTICLE = "favArticle"
 }
